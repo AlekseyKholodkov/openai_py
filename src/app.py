aaacf55571c5325c5f4576db32759308cfa8b155
Oleksii Kholodkov <Oleksii_Kholodkov@epam.com>
2024-10-06 16:25:21 +0200

    logging refactored

--- src/app.py
+++ src/app.py
@@ -5,12 +5,14 @@ from src.client.image_client import read_image
 from src.client.image_client import find_on_image
 from src.client.text_client import text_client
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename='app.log', encoding='UTF-8', filemode='w', level=logging.DEBUG,
+logging.basicConfig(filename='app.log', encoding='UTF-8', filemode='a', level=logging.DEBUG,
                     format='%(asctime)s | %(filename)s | %(message)s')
+logger = logging.getLogger(__name__)
 
 
 def application():
+    logger.info('application started')
     response_message = text_client()
     logger.info(f"text_client response_message.content={response_message}")
     # find_on_image("resources/img-1.jpg")
+    logger.info('application finished')
